726a6da261d7d2a3496392fd3026aab9c03a0b10
Bhagyashree Manglekar <bhagyashreemanglekar42@gmail.com>
2026-06-28 17:00:50 +0530

    add file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        // Change "5000" to match your local backend server port
+        target: 'http://localhost:8080', 
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
